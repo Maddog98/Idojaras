1774a24d3627b0d517db9a56064e38901bb57404
VBalazs <v.vbalazs@gmail.com>
2020-04-16 04:44:57 +0200

    megpróbáltam megcsinálni nem nem sikerült teljesen bele kéne rakni a város elemzést ,hogy jó várost írnak e be és akkor mmár jó lenne talán

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,14 @@
   >
     <meta http-equiv="refresh" content="20" />
     <main>
+      <div class="Attention">
+        <p>Figyelem a kereső csak létező városokra add eredményt!</p>
+      </div>
       <div class="search-box">
         <input
           type="text"
           class="search-bar"
-          placeholder="Search..."
+          placeholder="Keresett város..."
           v-model="query"
           @keypress="fetchWeather"
         />
@@ -19,14 +22,14 @@
 
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">
-            {{ weather.name }}, {{ weather.sys.country }}
-          </div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
         </div>
 
         <div class="weather-box">
           <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
           <div class="weather">{{ weather.weather[0].main }}</div>
+          <div class="hi_low"></div>
+          <div class="image"></div>
         </div>
       </div>
     </main>
@@ -42,15 +45,17 @@ export default {
       url_base: "https://api.openweathermap.org/data/2.5/",
       query: "",
       weather: {},
+      icon: ""
     };
   },
+
   methods: {
     fetchWeather(e) {
       if (e.key == "Enter") {
         fetch(
           `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
         )
-          .then((res) => {
+          .then(res => {
             return res.json();
           })
           .then(this.setResults);
@@ -58,8 +63,8 @@ export default {
     },
     setResults(results) {
       this.weather = results;
-    },
-  },
+    }
+  }
 };
 </script>
 
@@ -69,57 +74,52 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
-
+p {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
 body {
   font-family: "montserrat", sans-serif;
+  background-image: linear-gradient(purple, blue);
+  display: flex;
+  justify-content: center;
 }
-
 main {
   min-height: 100vh;
   padding: 25px;
-
-  background-image: linear-gradient(
-    to bottom,
-    rgba(0, 0, 0, 0.25),
-    rgba(0, 0, 0, 0.75)
-  );
+  width: 30vw;
 }
-
 #app {
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
 }
-
 .search-box {
   width: 100%;
   margin-bottom: 30px;
 }
-
 .search-box .search-bar {
   display: block;
   width: 100%;
   padding: 15px;
-
   color: black;
   font-size: 20px;
-
   appearance: none;
   outline: none;
   background: none;
-
-  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
-  background-color: burlywood;
-  border-radius: 0px 16px 0px 16px;
+  border-style: none;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 20px;
   transition: 0.4s;
 }
-
 .search-box .search-bar:focus {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
-  border-radius: 16px 0px 16px 0px;
+  border-radius: 20px;
 }
-
 .location-box .location {
   color: #fff;
   font-size: 32px;
@@ -154,6 +154,5 @@ main {
   font-size: 48px;
   font-weight: 700;
   font-style: italic;
-  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
